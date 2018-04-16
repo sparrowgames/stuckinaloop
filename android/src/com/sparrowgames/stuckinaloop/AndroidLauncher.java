98716faf400a95cb7b375a155a547ace14143dc5
Sparrow Games <developersparrowgames@gmail.com>
2018-04-16 20:00:56 +0200

    First Upload
    The first upload of a default LibGDX project.

---
+++ android/src/com/sparrowgames/stuckinaloop/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sparrowgames.stuckinaloop;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sparrowgames.stuckinaloop.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
